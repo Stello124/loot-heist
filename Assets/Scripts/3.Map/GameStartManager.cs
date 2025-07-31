6c0d50bd12b78c5b7e93283b4bd35e68450d5497
Nazar <nazarxguner@gmail.com>
2025-07-31 13:47:16 +0300

    Update
    - Yazı tutorial eklendi

---
+++ Assets/Scripts/3.Map/GameStartManager.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class GameStartManager : MonoBehaviour
+{
+    public TMP_Text introText;
+    public TMP_Text countdownText;
+    public float introDuration = 2.5f;
+
+    void Start()
+    {
+        StartCoroutine(StartSequence());
+    }
+
+    IEnumerator StartSequence()
+    {
+        introText.text = "Bomba elindeyken baþkasýna dokun! Süre bitmeden ver!";
+        introText.gameObject.SetActive(true);
+        countdownText.gameObject.SetActive(false);
+
+        yield return new WaitForSeconds(introDuration);
+
+        introText.gameObject.SetActive(false);
+        countdownText.gameObject.SetActive(true);
+
+        string[] countdownWords = { "3", "2", "1", "BAÞLA!" };
+        foreach (string word in countdownWords)
+        {
+            countdownText.text = word;
+            yield return new WaitForSeconds(1f);
+        }
+
+        countdownText.gameObject.SetActive(false);
+
+        // Güncel yöntemle GameManager'ý çaðýr
+        var controller = Object.FindFirstObjectByType<GameFlowController>();
+        if (controller != null)
+            controller.StartGame();
+    }
+}
+
+
